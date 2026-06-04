fea5c06f840d4f10d0688b02f4e4467939f56953
Willian Zanella <willianzanella99@gmail.com>
2026-06-03 21:37:54 -0300

    Fix

---
+++ Matrizes/ExePratico03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    int matriz[0][0];
+
+    printf("## Matriz 3x3 com 1 e 0 ##\n\n");
+    printf("A matriz resultante é:\n");
+    for (int l = 0; l < 3; l++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            if (l != c)
+            {
+                matriz[l][c] = 0;
+            }
+            else
+                matriz[l][c] = 1;
+            printf("[%d] ", matriz[l][c]);
+        }
+        printf("\n");
+    }
+    
+}
